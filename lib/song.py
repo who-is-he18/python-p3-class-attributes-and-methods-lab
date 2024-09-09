68fe03e61e01301c161c70cffb45fb8e17a66c58
geoffrey <geoffrey.waiganjo@student.moringaschool.com>
2024-09-09 13:36:25 +0300

    lab completion

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,54 @@
 class Song:
-    pass
+    # Class attributes
+    count = 0
+    genres = []
+    artists = []
+    genre_count = {}
+    artist_count = {}
+
+    def __init__(self, name: str, artist: str, genre: str):
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+
+        # Increment the song count
+        Song.add_song_to_count()
+
+        # Add the genre and artist to their respective lists
+        Song.add_to_genres(genre)
+        Song.add_to_artists(artist)
+
+        # Update genre and artist count
+        Song.add_to_genre_count(genre)
+        Song.add_to_artist_count(artist)
+
+    @classmethod
+    def add_song_to_count(cls):
+        cls.count += 1
+
+    @classmethod
+    def add_to_genres(cls, genre: str):
+        if genre not in cls.genres:
+            cls.genres.append(genre)
+
+    @classmethod
+    def add_to_artists(cls, artist: str):
+        if artist not in cls.artists:
+            cls.artists.append(artist)
+
+    @classmethod
+    def add_to_genre_count(cls, genre: str):
+        if genre in cls.genre_count:
+            cls.genre_count[genre] += 1
+        else:
+            cls.genre_count[genre] = 1
+
+    @classmethod
+    def add_to_artist_count(cls, artist: str):
+        if artist in cls.artist_count:
+            cls.artist_count[artist] += 1
+        else:
+            cls.artist_count[artist] = 1
+
+
+
